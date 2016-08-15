0d75c69ed3897b9d634efa6785e41c9624e5ab0e
hyendler <hyendler@gmail.com>
2016-08-15 13:36:40 -0700

    Added missing html

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,17 @@
 RECEIVE_URL='https://spreadsheets.google.com/feeds/list/1Ca9Grr_TcSyuU_i-s8rukzlRVMmyr8kwyDdLuDTBSQo/1/public/basic?alt=json';
-SUBMIT_URL="https://script.google.com/macros/s/AKfycbxSHmtqylt1ekdy4j761bFx7YW6xDr0BNvdht3xRY6bPgeFMaYY/exec";
+SUBMIT_URL="";
 
-//on document load, do all the things
 $(document).ready(function() {
 
-	//this is for RECEIVING DATA from Google Spreadsheets
-  // as soon as the html page loads, make an ajax call for loading data from Google Spreadsheet and then append it to the html
 	$.ajax({
       url: RECEIVE_URL,
       success: function(data){
-        //data is all the JSON that is returned from Google Spreadsheets from the ajax call
         console.log(data)
-        //call a function with the data in order to parse it and append it to the html
         readDataAndAppend(data);
       }
   })
+
+};
 
 	//this is SUBMITTING DATA to Google Spreadsheets
   //on submission of unicorn form, make an ajax call to send data to Google Spreadsheet
@@ -37,26 +34,22 @@ $(document).ready(function() {
 })
 
 
-//this is a helper function to parse through the received JSON and append it to the HTML because the Google Spreadsheet JSON is yucky to read through
 function readDataAndAppend(data){
-
-    //code to parse through JSON.  
-    var allUnicornData = [];
-    var cells = data.feed.entry;
-    
-    for (var i = 0; i < cells.length; i++){
-        var rowObj = {};
-        rowObj.timestamp = cells[i].title.$t;
-        var rowCols = cells[i].content.$t.split(',');
-        for (var j = 0; j < rowCols.length; j++){
-            var keyVal = rowCols[j].split(':');
-            rowObj[keyVal[0].trim()] = keyVal[1].trim();
-        }
-        allUnicornData.push(rowObj);
+  var allUnicornData = [];
+  var cells = data.feed.entry;
+  
+  for (var i = 0; i < cells.length; i++){
+    var rowObj = {};
+    rowObj.timestamp = cells[i].title.$t;
+    var rowCols = cells[i].content.$t.split(',');
+    for (var j = 0; j < rowCols.length; j++){
+        var keyVal = rowCols[j].split(':');
+        rowObj[keyVal[0].trim()] = keyVal[1].trim();
     }
-
-    //allUnicornData should be an array of objects with your data as key value pairs 
-    console.log(dataObjects)
+    allUnicornData.push(rowObj);
+  }
+  console.log(allUnicornData)
+}
 
 
     //code to parse through allUnicornData array and grab the data you need from key value pairs, and append it 
@@ -70,7 +63,7 @@ function readDataAndAppend(data){
     	var magicPowerText = "<p>" + unicornObject.magic + "</p>"
 
     	//add all the strings together into an html string that gets appended to a div that already exists on my HTML
-    	$("#my-unicorns").append(headerName + colorText + magicPowerText)
+    	$("#display-unicorns").append(headerName + colorText + magicPowerText)
     }
 }
 
